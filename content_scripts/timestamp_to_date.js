1744252fc27d069bda70af3e6ba28402864ece38
Pawel-Albert <wojciechowski.pawel.git@gmail.com>
2022-12-10 23:32:49 +0100

    timestamp to date change

--- content_scripts/timestamp_to_date.js
+++ content_scripts/timestamp_to_date.js
@@ -1,22 +1,28 @@
 ;(function () {
-  const timeStampToLocaleDate = timeStamp => {
-    return new Date(timeStamp).toLocaleDateString('pl-PL', {
-      timeZone: 'Europe/Warsaw',
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      timeZoneName: 'long'
-    })
-  }
-
-  console.log(
-    timeStampToLocaleDate(
-      Number(
-        prompt("Enter timestamp(example: '1669852799') to print date in console", '')
+  try {
+    const timeStampToLocaleDate = timeStamp => {
+      return new Date(timeStamp).toLocaleDateString('pl-PL', {
+        timeZone: 'Europe/Warsaw',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'long'
+      })
+    }
+    alert(
+      timeStampToLocaleDate(
+        Number(
+          prompt(
+            "Enter timestamp (example: '1669852799') to print date in console - to view it, open dev tools (F12)",
+            ''
+          )
+        )
       )
     )
-  )
+  } catch (err) {
+    alert(`Input proper timestamp ast this was and: ${err}`)
+  }
 })()
--- content_scripts/unlock_disabled.js
+++ content_scripts/unlock_disabled.js
@@ -1,10 +1,13 @@
 ;(function () {
   const tagi = document.getElementsByTagName('*')
   for (const tag of tagi) {
-    tag.removeAttribute('disabled')
+    if (tag.hasAttribute('disabled')) {
+      tag.removeAttribute('disabled')
+      tag.style = tag.style + '; border:2px dashed red;'
+    }
     if (tag.classList.contains('disabled')) {
       tag.classList.remove('disabled')
-      tag.style = tag.style + '; border:7px dashed purple;'
+      tag.style = tag.style + '; border:4px dashed purple;'
     }
   }
   console.log(`%c Unlocked`, 'font-family:monospace; color:firebrick;font-size:25px')
